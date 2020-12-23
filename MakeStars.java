87118bfd261ba3714f776c5a029a0fa74de1f8c7
OliverSantiago <osantiagokermani20@stuy.edu>
2020-12-23 14:15:42 -0500

    Initial

---
+++ MakeStars.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class MakeStars{
+  public static void main( String[]args ){
+      //use the standard input (terminal input)
+      //as the string that you read from
+      Scanner n = new Scanner( System.in );
+      //use hasNextLine()/nextLine() to loop over
+      //all of the data
+      while (n.hasNextLine()){
+        Scanner line = new Scanner(n.nextLine());
+          while (line.hasNext()){
+
+          }
+        }
+      //If you want to read the input word by word
+      //this can be replaced with hasNext() and next()
+
+  }
+}
