3e0dd5a9e01f2f504016de2465c4b8dac954ca54
OliverSantiago <osantiagokermani20@stuy.edu>
2020-12-23 14:23:46 -0500

    Stars work so far!

--- MakeStars.java
+++ MakeStars.java
@@ -9,8 +9,15 @@ public class MakeStars{
       while (n.hasNextLine()){
         Scanner line = new Scanner(n.nextLine());
           while (line.hasNext()){
-
+            String temp = line.next();
+            String answer = "";
+            for (int i = 0; i < temp.length(); i++){
+              answer+="*";
+            }
+            System.out.print(answer);
+            System.out.print(" ");
           }
+          System.out.println();
         }
       //If you want to read the input word by word
       //this can be replaced with hasNext() and next()
