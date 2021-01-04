870956cdea0c7e17667d4aee333c9bebf73d4126
OliverSantiago <osantiagokermani20@stuy.edu>
2021-01-04 13:41:38 -0500

    intial method with testing seems to work

--- PigLatin.java
+++ PigLatin.java
@@ -1,6 +1,9 @@
 import java.util.*;
 public class PigLatin{
   public static void main( String[]args ){
+    System.out.println(pigLatinSimple("hello"));
+    System.out.println(pigLatinSimple("mock"));
+    System.out.println(pigLatinSimple("aaron"));
   }
 
   public static String pigLatinSimple(String s){
